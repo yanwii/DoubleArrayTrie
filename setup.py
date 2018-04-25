a3cf40ffdad911f1951f40b7e3230a3aaceba974
wei.yan <wei.yan@socialcredits.cn>
2018-04-25 14:22:56 +0800

    update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extension_mod = Extension("pydat.pydat",
                         extra_compile_args=['-std=c++11'])
 
 setup(name='pydat',
-      version='0.3.1',
+      version='0.3.2',
       keywords = ("Double Array Trie", "DAT"),  
       description = "DoubleArrayTrie(DAT) support prefix search & exact search & multiple pattern match for python implemented by c++",  
       long_description = "",  
