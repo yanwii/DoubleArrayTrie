2cbd0b5987940bf9a2130a64198ed0802388551b
wei.yan <wei.yan@socialcredits.cn>
2018-08-08 11:51:42 +0800

    v0.4.9

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ package_data={"pydat":["libboost_python2.so.1.66.0"]},
 if sys.version.split(".")[0] == '2':
       py_version = 2
       package_data = {"pydat":["libboost_python2.so.1.66.0"]}
+      libraries = ['boost_python']
 elif sys.version.split(".")[0] == '3':
       py_version = 3
-      package_data={"pydat":["libboost_python3.so.1.66.0"]}
+      package_data = {"pydat":["libboost_python3.so.1.66.0"]}
+      libraries = ['boost_python3']
 
 runtime_dirs = [i + "/pydat" for i in site.getsitepackages()]
 
@@ -22,15 +24,18 @@ extension_mod = Extension("pydat.pydat",
                         include_dirs=['pydat/src'],
                         library_dirs=['pydat/pydat'],
                         runtime_library_dirs=runtime_dirs,
-                        libraries=['boost_python'],
+                        libraries=libraries,
                         extra_compile_args=['-std=c++11'])
 
 long_des = ""
-with open("./README.rst") as fopen
-      long_des = fopen.read()
+try:
+      with open("./README.rst") as fopen:
+            long_des = fopen.read()
+except Exception:
+      pass
 
 setup(name='pydat',
-      version='0.4.7',
+      version='0.4.9',
       keywords = ("Double Array Trie", "DAT"),  
       description = "DoubleArrayTrie(DAT) support prefix search & exact search & multiple pattern match for python implemented by c++",  
       long_description = long_des,  
